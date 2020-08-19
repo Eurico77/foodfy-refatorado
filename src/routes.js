daa02140aba9b9ada10567387dab5b9e91822f17
eurico77 <euricomagalhaes61@gmail.com>
2020-08-19 09:20:51 -0300

    ok

--- src/routes.js
+++ src/routes.js
@@ -4,29 +4,22 @@ const recipes = require("./data");
 
 routes.get("/", (req, res) => {
   return res.render("home", { items: recipes });
-
 });
 
 routes.get("/sobre", (req, res) => {
   return res.render("sobre");
-
 });
 
 routes.get("/recipes", (req, res) => {
   return res.render("recipes", { items: recipes });
-
 });
 
 routes.get("/recipes/:index", (req, res) => {
-  const recipeData = [...recipes]; // Array de receitas carregadas do data.js
+  const recipeData = [...recipes];
   const recipeIndex = req.params.index;
   const recipe = recipeData[recipeIndex];
 
   return res.render("recipe", { items: recipe });
 });
-
-// routes.use(function (req, res) {
-//   res.status(404).render("not-found");
-// });
 
 module.exports = routes;
